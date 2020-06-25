868155454945b23aa9a02d35414ec6053a38a615
오형남 <zzzsh789@naver.com>
2020-06-25 09:42:10 +0900

    Add: Robot.js

---
+++ src/Robot.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const styles = {
+  width: '50%',
+  height: '50%',
+  backgroundColor: 'black',
+}
+const Robot = () => {
+  return (
+    <>
+      <div style={styles}>
+      </div>
+    </>
+  )
+}
+
+export default Robot;
